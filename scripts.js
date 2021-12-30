607f795e3abcce96a45796f742734860ebc4315e
Sachin Sankonatti <42653952+SachinMS4@users.noreply.github.com>
2021-12-30 19:09:34 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 let insertText = document.getElementById("text");
+
 let text = "It works on my machine!";
 let index = 0;
 
-(function writeText() {
+function writeText() {
   insertText.textContent = text.slice(0, index);
 
   index++;
 
-  if (index > text.length) return;
+  if (index > text.length) 
+    return;
   setTimeout(writeText, 250);
-})();
+};
+
+writeText();
