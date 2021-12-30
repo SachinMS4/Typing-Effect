b4f2ac1fd8d34590d6d87e14eec899b26d76a1b0
Sachin Sankonatti <42653952+SachinMS4@users.noreply.github.com>
2021-12-30 19:02:45 +0530

    Typing Effect JavaScript file.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let insertText = document.getElementById("text");
+let text = "It works on my machine!";
+let index = 0;
+
+(function writeText() {
+  insertText.textContent = text.slice(0, index);
+
+  index++;
+
+  if (index > text.length) return;
+  setTimeout(writeText, 250);
+})();
